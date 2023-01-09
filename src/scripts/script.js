93db76ee6b9ac85382e05c298ca2cc76d293e527
Juscélia de Souza <jusceliadesousa@gmail.com>
2023-01-09 19:52:44 -0300

    imagem modo light desativada

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,15 +3,15 @@ function toggleMode() {
   html.classList.toggle("light");
 
   // pegar a tag img
-  const img = document.querySelector(".profile img");
+  // const img = document.querySelector(".profile img");
 
   // substituir img
-  if (html.classList.contains('light')) {
-    // estando no light mode, adicionar a img tema light
-    img.setAttribute('src', 'https://github.com/jusceliadesouza.png')
-  } else {
-    // estando no modo normal, manter a img normal
-    img.setAttribute('src', 'https://media.licdn.com/dms/image/C4E03AQEE0iAn9FkbaQ/profile-displayphoto-shrink_200_200/0/1647622947251?e=1677715200&v=beta&t=7vunbl0wekpUa8A2_-5BXT_jSbdh2Os3a-ywXuzkNNg')
-  }
+  // if (html.classList.contains('light')) {
+  //   // estando no light mode, adicionar a img tema light
+  //   img.setAttribute('src', 'https://github.com/jusceliadesouza.png')
+  // } else {
+  //   // estando no modo normal, manter a img normal
+  //   img.setAttribute('src', 'https://media.licdn.com/dms/image/C4E03AQEE0iAn9FkbaQ/profile-displayphoto-shrink_200_200/0/1647622947251?e=1677715200&v=beta&t=7vunbl0wekpUa8A2_-5BXT_jSbdh2Os3a-ywXuzkNNg')
+  // }
 
 }
